a15b55d935622f09193ea060a81800b0a2e40e6e
Alicia <tl2180@nyu.edu>
2020-04-14 21:48:20 +0800

    rctma 2.0

--- Validation_Code/RCTMA.py
+++ Validation_Code/RCTMA.py
@@ -6,26 +6,29 @@
 
 # how to initialize tMatij_np1 and tMatij_n
 
+import time
 import numpy as np
 from copy import deepcopy
 
-n1 = 1
-n2 = 2
-nbp = 3
-# vscatterer[(f)-1]: nf*nf
-vscatterer = [np.ones((n + 1, n + 1)) for n in range(nbp)]
 
-# hij[findice(j,f)]: nj*nf
-hij = [[np.random.rand(j + 1, i + 1) for i in range(nbp)] for j in range(nbp)]
+def rctma(num):
 
-# hij = [[np.random.rand(n1, n1), np.random.rand(n1, n2)], # (n1, nf)
-#        [np.random.rand(n2, n1), np.random.rand(n2, n2)]]
+    n1 = 1
+    n2 = 2
+    nbp = num
+    # vscatterer[(f)-1]: nf*nf
+    vscatterer = [np.ones((n + 1, n + 1)) for n in range(nbp)]
 
-tMatij_n = [[0 for i in range(nbp)] for j in range(nbp)]
+    # hij[findice(j,f)]: nj*nf
+    hij = [[np.random.rand(j + 1, i + 1) for i in range(nbp)] for j in range(nbp)]
 
-tMatij_np1 = [[0 for i in range(nbp)] for j in range(nbp)]
+    # hij = [[np.random.rand(n1, n1), np.random.rand(n1, n2)], # (n1, nf)
+    #        [np.random.rand(n2, n1), np.random.rand(n2, n2)]]
 
-def rctma():
+    tMatij_n = [[0 for i in range(nbp)] for j in range(nbp)]
+
+    tMatij_np1 = [[0 for i in range(nbp)] for j in range(nbp)]
+
     # ========================================================================
     # CALCULATION OF THE TWO FIRST RELATION : 2 PARTICLES SYSTEM (N=2)
     # ========================================================================
@@ -34,22 +37,18 @@ def rctma():
     # Trans1 = vscatterer[(2)-1] * hij[findice(2,1)].g_PointerToMatrix() //2*2 x 2*1 ->2*1
     # Trans2 = hij[findice(1,2)] * Trans1 // 1*2 x 2*1 = 1*1
     # Trans1 = vscatterer[(1)-1] * Trans2 // n1*n1 
-    Trans1 = vscatterer[(1)-1] * hij[(1)-1][(2)-1] * vscatterer[(2)-1] * hij[(2)-1][(1)-1]
-    print(vscatterer[(1)-1].shape, hij[(1)-1][(2)-1].shape, vscatterer[(2)-1].shape, hij[(2)-1][(1)-1].shape)
-    print(vscatterer[(1)-1] * hij[(1)-1][(2)-1] * vscatterer[(2)-1])
-    print(Trans1)
+    Trans1 = vscatterer[(1)-1] @ hij[(1)-1][(2)-1] @ vscatterer[(2)-1] @ hij[(2)-1][(1)-1]
     # Trans2.s_MatUmoinsB(Trans1)
     # Trans2.selfInvertMatrix(info) // n1*n1
     Trans2 = np.linalg.inv(np.identity(n1) - Trans1)
     # tMatij_n[findice(1,1)] = Trans2 * vscatterer[(1)-1] // n1*n1
-    print(Trans2.shape, vscatterer[(1)-1].shape )
-    tMatij_n[(1)-1][(1)-1] = Trans2 * vscatterer[(1)-1]
+    tMatij_n[(1)-1][(1)-1] = Trans2 @ vscatterer[(1)-1]
      
     # ETAPE 2: CALCUL DE LA MATRICE T [(1,2) 2]
     # ========================================================================
     # Trans1 =  hij[findice(1,2)].g_PointerToMatrix() * vscatterer[(2)-1] ;
     # tMatij_n[findice(1,2)] = tMatij_n[findice(1,1)] * Trans1 ;
-    tMatij_n[(1)-1][(2)-1] = tMatij_n[(1)-1][(1)-1] * hij[(1)-1][(2)-1] * vscatterer[(2)-1] 
+    tMatij_n[(1)-1][(2)-1] = tMatij_n[(1)-1][(1)-1] @ hij[(1)-1][(2)-1] @ vscatterer[(2)-1] 
      
     # ETAPE 3: CALCUL DE LA MATRICE T [(2,2) 2]
     # ========================================================================
@@ -59,7 +58,7 @@ def rctma():
     # Trans2 = hij[findice(2,1)] * Trans1 ;
     # dim: n2*n2 * n2*n2 = n2*n2
     # Trans1 = vscatterer[(2)-1] * Trans2 ;  ???
-    Trans1 = vscatterer[(2)-1] * hij[(2)-1][(1)-1] * vscatterer[(1)-1] * hij[(1)-1][(2)-1]
+    Trans1 = vscatterer[(2)-1] @ hij[(2)-1][(1)-1] @ vscatterer[(1)-1] @ hij[(1)-1][(2)-1]
     # calcul de I-mat: dim: n2*n2
     # Trans2.s_MatUmoinsB(Trans1) ;
     # Inversion de la matrice: n2*n2
@@ -67,7 +66,7 @@ def rctma():
     Trans2 = np.linalg.inv(np.identity(n2) - Trans1)
     # dim: n2*n2 * n2*n2 = n2*n2
     # tMatij_n[findice(2,2)] = Trans2 * vscatterer[(2)-1] ;
-    tMatij_n[(2)-1][(2)-1] = Trans2 * vscatterer[(2)-1]
+    tMatij_n[(2)-1][(2)-1] = Trans2 @ vscatterer[(2)-1]
 
     # ETAPE 4: CALCUL DE LA MATRICE T [(2,1) 2]
     # ========================================================================
@@ -76,21 +75,16 @@ def rctma():
     # Trans1 = hij[findice(2,1)].g_PointerToMatrix() * vscatterer[(1)-1]
     # dim: n2*n2* * n2*n1 = n2*n1
     # tMatij_n[findice(2,1)] = tMatij_n[findice(2,2)] * Trans1 ;
-    tMatij_n[(2)-1][(1)-1] = tMatij_n[(2)-1][(2)-1] * hij[(2)-1][(1)-1] * vscatterer[(1)-1] 
+    tMatij_n[(2)-1][(1)-1] = tMatij_n[(2)-1][(2)-1] @ hij[(2)-1][(1)-1] @ vscatterer[(1)-1] 
 
-    # if (nbp==2)	{
-    # tMatij_np1[findice(1,1)] = tMatij_n[findice(1,1)] ;
-    # tMatij_np1[findice(1,2)] = tMatij_n[findice(1,2)] ;
-    # tMatij_np1[findice(2,1)] = tMatij_n[findice(2,1)] ;
-    # tMatij_np1[findice(2,2)] = tMatij_n[findice(2,2)] ;
-    # }
-
+    # print("0,0", tMatij_n[0][0].shape, tMatij_n[0][0])
+    # print("0,1", tMatij_n[0][1].shape, tMatij_n[0][1])
+    # print("1,0", tMatij_n[1][0].shape, tMatij_n[1][0])
+    # print("1,1", tMatij_n[1][1].shape, tMatij_n[1][1])
     if nbp < 2:
         raise ValueError
     if nbp == 2:
-        print(tMatij_n)
         return tMatij_n
-
     # ===========================================================================
     # ===========================================================================
     #     	GENERAL CASE WHEN N > 2
@@ -107,17 +101,18 @@ def rctma():
 
         # Trans4 = cMatrice(vscatterer[(f)-1].g_dimtn(),
         # vscatterer[(f)-1].g_dimtn() );
-        Trans1 = np.zeros((vscatterer[(f)-1].ndim, vscatterer[(f)-1].ndim)) # ???
+        Trans1 = np.zeros((vscatterer[(f)-1].shape[0], vscatterer[(f)-1].shape[0])) # ???
         for i in range(1, f):
-            Trans2 = np.zeros((vscatterer[(i)-1].ndim, vscatterer[(f)-1].ndim))
+            Trans2 = np.zeros((vscatterer[(i)-1].shape[0], vscatterer[(f)-1].shape[0]))
             for j in range(1, f):
-                Trans2 += tMatij_n[(i)-1][(j)-1] * hij[(j)-1][(f)-1]
-            Trans1 += hij[(f)-1][(i)-1] * Trans2
+                # print("i loop", "i - 1", i - 1, "j - 1", j - 1, "f - 1",f - 1)
+                Trans2 += tMatij_n[(i)-1][(j)-1] @ hij[(j)-1][(f)-1]
+            Trans1 += hij[(f)-1][(i)-1] @ Trans2
         
-        Trans3 = vscatterer[(f)-1] * Trans1
-        Trans4 = np.linalg.inv(np.identity(Trans3.ndim) - Trans3)
+        Trans3 = vscatterer[(f)-1] @ Trans1
+        Trans4 = np.linalg.inv(np.identity(Trans3.shape[0]) - Trans3)
 
-        tMatij_np1[(f)-1][(f)-1] = Trans4 * vscatterer[(f)-1]
+        tMatij_np1[(f)-1][(f)-1] = Trans4 @ vscatterer[(f)-1]
 
         # ===========================================================================
         #    			CALCULATION OF THE RELATION N2
@@ -130,11 +125,11 @@ def rctma():
         for j in range (1, f):
             # Trans4 = cMatrice (vscatterer[(f)-1].g_dimtn(),
             # vscatterer[(j)-1].g_dimtn() )
-            Trans4 = np.zeros((vscatterer[(f)-1].ndim, vscatterer[(j)-1].ndim))
+            Trans4 = np.zeros((vscatterer[(f)-1].shape[0], vscatterer[(j)-1].shape[0]))
             for i in range (1, f):
-                Trans4 += hij[(f)-1][(i)-1] * tMatij_n[(i)-1][(j)-1]
+                Trans4 += hij[(f)-1][(i)-1] @ tMatij_n[(i)-1][(j)-1]
 
-            tMatij_np1[(f)-1][(j)-1] = tMatij_np1[(f)-1][(f)-1] * Trans4
+            tMatij_np1[(f)-1][(j)-1] = tMatij_np1[(f)-1][(f)-1] @ Trans4
     
         # ===========================================================================
         #     			CALCULATION OF THE RELATION N3			   
@@ -145,14 +140,17 @@ def rctma():
 
         for k in range (1, f):
             for i in range (1, f + 1):
-                Trans1 = np.zeros((vscatterer[(k)-1].ndim, vscatterer[(f)-1].ndim))
+                Trans1 = np.zeros((vscatterer[(k)-1].shape[0], vscatterer[(f)-1].shape[0]))
                 for j in range (1, f):
-                    Trans1 += hij[(k)-1][(j)-1] * tMatij_n[(j)-1][(f)-1]
+                    # print("f", f,"k", k,"i", i)
+                    # print("tMatij_n[(k)-1][(j)-1]", tMatij_n[(k)-1][(j)-1].shape,
+                    #     "hij[(j)-1][(f)-1]", hij[(j)-1][(f)-1].shape, "Trans1", Trans1.shape)
+                    Trans1 +=  tMatij_n[(k)-1][(j)-1] @ hij[(j)-1][(f)-1]
 
                 if i == f:
-                    tMatij_np1[(k)-1][(f)-1] = Trans1 * tMatij_np1[(f)-1][(f)-1]
+                    tMatij_np1[(k)-1][(f)-1] = Trans1 @ tMatij_np1[(f)-1][(f)-1]
                 else:
-                    Trans2 = Trans1 * tMatij_np1[(f)-1][(i)-1]
+                    Trans2 = Trans1 @ tMatij_np1[(f)-1][(i)-1]
                     tMatij_np1[(k)-1][(i)-1] = tMatij_n[(k)-1][(i)-1] + Trans2
 
         # ===========================================================================
@@ -163,11 +161,11 @@ def rctma():
         # ===========================================================================
 
         for k in range(1, f):
-            Trans4 = np.zeros((vscatterer[(k)-1].ndim, vscatterer[(f)-1].ndim))
+            Trans4 = np.zeros((vscatterer[(k)-1].shape[0], vscatterer[(f)-1].shape[0]))
             for j in range(1, f):
-                Trans3 = tMatij_n[k-1][j-1] * hij[j-f][f-1]
-                Trans4 = Trans4  + Trans3
-            tMatij_np1[k-1][f-1] = Trans4 * tMatij_np1[f-1][f-1]
+                # print("tMatij_n[k-1][j-1]", tMatij_n[k-1][j-1].shape, )
+                Trans4 += tMatij_n[k-1][j-1] @ hij[j-1][f-1]
+            tMatij_np1[k-1][f-1] = Trans4 @ tMatij_np1[f-1][f-1]
 
         # Preparation of the new recurrence
         # ===========================================================================
@@ -175,5 +173,7 @@ def rctma():
             for j in range(1, f+1):
                 tMatij_n[i-1][j-1] = tMatij_np1[i-1][j-1]
 
-
-rctma()
+time1 = time.time()
+rctma(40)
+time2 = time.time()
+print("time:", time2 - time1)
